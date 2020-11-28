9f643fbfc9e8c851b43b3f4f2176b60a51883bf6
shashi <shashi2216@gmail.com>
2020-11-28 13:35:57 +0530

    comment added in login page

--- PracticeProject/src/test/java/com/Pages/LoginPage.java
+++ PracticeProject/src/test/java/com/Pages/LoginPage.java
@@ -1,3 +1,4 @@
+//login page details
 package com.Pages;
 
 //import org.openqa.selenium.By;
@@ -7,6 +8,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
 	WebDriver driver;
+	
 	
 	public LoginPage(WebDriver ldriver)
 	{
